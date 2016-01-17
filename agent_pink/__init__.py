fbb5b5248051d85f00693d651bce7f0a69019f28
George F Rogers <grogers385@gmail.com>
2016-01-17 15:14:25 -0600

    update

--- agent_pink/__init__.py
+++ agent_pink/__init__.py
@@ -5,6 +5,7 @@ from . import handlers
 app = web.Application([
     (r"/", handlers.Articles),
     (r"/articles/", handlers.Articles),
-    (r"/articles/([^/]+)", handlers.Article),
+    (r"/articles/(.+)", handlers.Article),
     (r"/static/(.+)", web.StaticFileHandler, {"path": "static"}),
+    (r"/(favicon.ico)", web.StaticFileHandler, {"path": "static"}),
 ])
